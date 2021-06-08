21b8736ef2d1e8fbc9aff7f828ca8079741dfb62
AndresAco92 <andres.antonio.aco92@gmail.com>
2021-06-08 16:14:08 +0200

    working

---
+++ assets/js/scrypt.js
@@ -0,0 +1,7 @@
+const app = new VTTCue({
+
+  el: '#root',
+  data:{
+    
+  }
+});
---
+++ index-php.php
@@ -0,0 +1,25 @@
+<?php
+    require_once __DIR__ . '/data/db.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/style/style.css">
+  <title>PHP-Dischi</title>
+</head>
+<body>
+
+  <?php
+
+    var_dump($covers);
+
+  ?>
+
+
+
+</body>
+</html>
